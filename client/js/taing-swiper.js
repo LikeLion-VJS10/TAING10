5dba044f00c244b16dc36250a780289ae0da06d7
s-ja <dkstmdwl0615@naver.com>
2023-02-02 10:59:35 +0900

    Feat : hero 슬라이드 탑제 및 기능구현, 추가작업 필요

--- client/js/taing-swiper.js
+++ client/js/taing-swiper.js
@@ -10,3 +10,39 @@ export const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev' // 이번 버튼 클래스명
   }
 })
+
+const hero = new Swiper('.hero', {
+  // cssMode: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  keyboard: {
+    enabled: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  }
+})
+
+const event = new Swiper('.event', {
+  // cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+  spaceBetween: 30,
+  freeMode: true
+})
