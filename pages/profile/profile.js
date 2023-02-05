f8cd443fecdd0eaf340d63c1fdcbee1785627f53
sykang013 <sy.kang013@gmail.com>
2023-02-05 23:31:45 +0900

    #64 Feat : 편집버튼 클릭시 텍스트 및 버튼 스타일 변경

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -2,10 +2,13 @@ const mainTxt = document.querySelector('.profile-title__main');
 const subTxt = document.querySelector('.profile-title__sub');
 const editBtn = document.querySelector('.profile-edit__button');
 const editImg = Array.from(document.querySelectorAll('.profile-img-list i'));
-console.log(editImg);
+const activeProfile = document.querySelector('.active');
+console.log(activeProfile)
+
 
 function onEdit() {
-  editBtn.style.cssText="color:black; background-color: white; ";
+  if(!!(editImg[0].classList.contains('fa-lock'))){
+  editBtn.style.cssText = "color:black; background-color:white; font-weight: 700";
   editBtn.textContent = "완료";
   mainTxt.textContent = "프로필 편집";
   subTxt.textContent = "편집할 프로필을 선택해주세요.";
@@ -13,8 +16,27 @@ function onEdit() {
     function(currentElement, index){
       currentElement.classList.toggle('fa-lock');
       currentElement.classList.toggle('fa-pencil');
-    }
-  )
+      }
+    )
+    activeProfile.classList.toggle('a11yHidden')
+    // activeProfile.classList.toggle('profile-img__wrap--shadow')
+    return console.log("1")
+  }
+  else if(!!(editImg[0].classList.contains('fa-pencil'))){
+    editBtn.style.cssText = "var(--gray-300); background-color:inherit;";
+    editBtn.textContent = "프로필 편집";
+    mainTxt.textContent = "프로필 선택";
+    subTxt.textContent = "시청할 프로필을 선택해주세요.";
+    editImg.forEach(
+      function(currentElement, index){
+        currentElement.classList.toggle('fa-lock');
+        currentElement.classList.toggle('fa-pencil');
+        }
+      )
+      activeProfile.classList.toggle('a11yHidden')
+      
+    return console.log("2")
+  }
 }
 
 editBtn.addEventListener('click', ()=>{
