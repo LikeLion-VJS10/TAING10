5c1a6b562b6ddd2eb05fa70cfd31dc2939c3debf
sykang013 <sy.kang013@gmail.com>
2023-02-05 18:32:20 +0900

    Feat : 마우스 호버 애니메이션 완료

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -8,18 +8,18 @@
 const mainTxt = document.querySelector('.profile-title__main');
 const subTxt = document.querySelector('.profile-title__sub');
 const editBtn = document.querySelector(".profile-edit__button");
+const editPencil = document.querySelector(".fa-lock");
 
+console.log(document.querySelector(".fa-lock"))
 //사용자가 편집하기 버튼을 클릭했을때 실행되는 함수
 function onEdit() {
   console.log("test");
-  // 버튼 스타일 변경
-  editBtn.style.cssText="color:black; background-color: white;"
+  editBtn.style.cssText="color:black; background-color: white; "
   editBtn.textContent = "완료";
-  // 텍스트 변경
   mainTxt.textContent = "프로필 편집"
   subTxt.textContent = "편집할 프로필을 선택해주세요."
-  //메인,서브 텍스트 변경됨(txt만 바뀜)) click
   //자물쇠가 연필이미지로 바뀜(폰트어썸) click
+  editPencil.className = ".fa-pencil"
 }
 
 //문서객체.addEventListener(이벤트 이름, 함수)
@@ -27,5 +27,7 @@ function onEdit() {
 editBtn.addEventListener('click', ()=>{
   onEdit();
 });
+
+
     //챌1-버튼에 마우스 호버시 버튼 포커스 효과 mouseover
     //챌2-프로필 이미지에 마우스 호버시 위로 살짝 올라가는 애니메이션 mouseover
