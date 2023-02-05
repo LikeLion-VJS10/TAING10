7a4e0ff787eaf91efda9095365ff8e91eb584d35
sykang013 <sy.kang013@gmail.com>
2023-02-05 15:08:05 +0900

    #50 Fix : 클래스네임 BEM방식으로 수정

---
+++ pages/profile/profile.js
@@ -0,0 +1,12 @@
+//필요한 돔 요소 정의하기
+
+const editBtn = dovument.querySelector('')
+
+function onClick() {
+  //버튼 클릭시 버튼 스타일 바뀜
+  //메인,서브 텍스트 
+  //자물쇠가 연필이미지로
+  //
+}
+
+onClick.addEventListener('click')
