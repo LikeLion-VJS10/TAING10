755d8d0412311c4d27d8073037ab381c7f3af0e7
simyuseon <yseon0508@naver.com>
2023-02-06 17:55:11 +0900

    Feat : swiper json 데이터로 적용

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,93 @@
-import { swiper } from './taing-swiper.js'
+let list = document.querySelector('.contents__list')
 
-// const swiper = new Swiper(".mySwiper", {
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
+function settingSwipers() {
+  let swiper = new Swiper('.slow', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+
+    autoplay: {
+      delay: 0 //add
+    },
+    speed: 7000,
+    loop: true,
+    allowTouchMove: false, //드래그 슬라이드 막음
+    loopAdditionalSlides: 1,
+    loopFillGroupWithBlank: true
+  })
+
+  let swiper2 = new Swiper('.fast', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+
+    autoplay: {
+      delay: 0 //add
+    },
+    speed: 6000,
+    loop: true,
+    allowTouchMove: false, //드래그 슬라이드 막음
+    loopAdditionalSlides: 1,
+    loopFillGroupWithBlank: true
+  })
+}
+
+async function tivingFun() {
+  let response = await fetch('http://localhost:3000/tivingFun')
+    .then(response => response.json())
+    .then(data =>
+      data.map(item => {
+        let onLi = document.createElement('li')
+        let onImg = document.createElement('img')
+        onLi.classList.add('contents__item')
+        onImg.setAttribute('src', `${item.image}`)
+        onImg.setAttribute('alt', `${item.description}`)
+        onLi.appendChild(onImg)
+        list.appendChild(onLi)
+      })
+    )
+    .then(() => {
+      settingSwipers()
+    })
+}
+tivingFun()
+
+let doubleList = document.querySelector('.slow-swiper')
+console.log(doubleList)
+async function wantFun1() {
+  let response = await fetch('http://localhost:3000/wantFun')
+    .then(response => response.json())
+    .then(data =>
+      data.map(item => {
+        let onLi = document.createElement('li')
+        let onImg = document.createElement('img')
+        onLi.classList.add('swiper-slide')
+        onImg.setAttribute('src', `${item.image}`)
+        // onImg.setAttribute('alt', `${item.description}`)
+        onLi.appendChild(onImg)
+        doubleList.appendChild(onLi)
+      })
+    )
+}
+wantFun1()
+
+let doubleList2 = document.querySelector('.fast-swiper ')
+console.log(doubleList2)
+
+async function wantFun2() {
+  let response = await fetch('http://localhost:3000/wantFun')
+    .then(response => response.json())
+    .then(data =>
+      data.map(item => {
+        let onLi = document.createElement('li')
+        let onImg = document.createElement('img')
+        onLi.classList.add('swiper-slide')
+        onImg.setAttribute('src', `${item.image}`)
+        // onImg.setAttribute('alt', `${item.description}`)
+        onLi.appendChild(onImg)
+        doubleList2.appendChild(onLi)
+      })
+    )
+    .then(() => {
+      settingSwipers()
+    })
+}
+wantFun2()
--- client/js/onboarding-swiper.js
+++ client/js/onboarding-swiper.js
@@ -1,26 +1,83 @@
-let swiper = new Swiper('.slow', {
-  slidesPerView: 5,
-  spaceBetween: 10,
+// 스와이퍼 라이브러리 설정
+function settingSwipers() {
+  let swiper = new Swiper('.slow', {
+    slidesPerView: 5,
+    spaceBetween: 10,
 
-  autoplay: {
-    delay: 0 //add
-  },
-  speed: 7000,
-  loop: true,
-  allowTouchMove: false, //드래그 슬라이드 막음
-  loopAdditionalSlides: 1,
-  loopFillGroupWithBlank: true
-})
-let swiper2 = new Swiper('.fast', {
-  slidesPerView: 5,
-  spaceBetween: 10,
+    autoplay: {
+      delay: 0 //add
+    },
+    speed: 7000,
+    loop: true,
+    allowTouchMove: false, //드래그 슬라이드 막음
+    loopAdditionalSlides: 1,
+    loopFillGroupWithBlank: true
+  })
 
-  autoplay: {
-    delay: 0 //add
-  },
-  speed: 6000,
-  loop: true,
-  allowTouchMove: false, //드래그 슬라이드 막음
-  loopAdditionalSlides: 1,
-  loopFillGroupWithBlank: true
-})
+  let swiper2 = new Swiper('.fast', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+
+    autoplay: {
+      delay: 0 //add
+    },
+    speed: 6000,
+    loop: true,
+    allowTouchMove: false, //드래그 슬라이드 막음
+    loopAdditionalSlides: 1,
+    loopFillGroupWithBlank: true
+  })
+}
+
+function renderMovieList() {
+  // 동적으로 가져온 영화 데이터를 화면에 렌더링
+}
+
+function fetchMovieData() {
+  // 서버에 영화 데이터 요청
+  // 프로미스 객체 반환
+  return fetch('....')
+}
+
+export function init() {
+  fetchMovieData()
+    .then(() => {
+      return renderMovieList()
+    })
+    .then(() => {
+      settingSwipers()
+    })
+}
+
+// init()
+
+// function assignSlowSwiper(target) {
+//   let swiper = new Swiper(target, {
+//     slidesPerView: 5,
+//     spaceBetween: 10,
+
+//     autoplay: {
+//       delay: 0 //add
+//     },
+//     speed: 7000,
+//     loop: true,
+//     allowTouchMove: false, //드래그 슬라이드 막음
+//     loopAdditionalSlides: 1,
+//     loopFillGroupWithBlank: true
+//   })
+// }
+
+// function renderMovieData() {}
+
+// async function fetchMovieData() {
+//   try {
+//     const res = await fetch('....')
+//     const data = await res.json()
+//     const slowSliderContainer = renderMovieList(data)
+//     assignSlowSwiper(slowSliderContainer)
+//   } catch (error) {
+//     console.error(error.message)
+//   }
+// }
+
+// // assignSlowSwiper()
