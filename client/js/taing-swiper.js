73ae7076d5ff38d20f06532cf20aee1662d50f54
seovee <jinseob102@naver.com>
2023-02-07 14:24:15 +0900

    Feat : taing swiper 반응형 추가

--- client/js/taing-swiper.js
+++ client/js/taing-swiper.js
@@ -1,27 +1,45 @@
 export const swiperSlide = new Swiper('.contents__inner', {
-  slidesPerView: 5,
-  slidesPerGroup: 5,
-
+  slidesPerView: 2,
+  slidesPerGroup: 2,
   keyboard: {
     enabled: true
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 4
+    },
+    1024: {
+      slidesPerView: 5,
+      slidesPerGroup: 6
+    }
   }
 })
 
 // ^ 티빙에서 꼭 봐야하는 콘텐츠 영역 Swiper
 export const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 8,
-  slidesPerGroup: 8,
+  slidesPerView: 3,
+  slidesPerGroup: 3,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
   keyboard: {
     enabled: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+      slidesPerGroup: 5
+    },
+    1024: {
+      slidesPerView: 8,
+      slidesPerGroup: 8
+    }
   }
   // autoplay: {
   // delay: 2500,
@@ -31,27 +49,47 @@ export const swiper = new Swiper('.mySwiper', {
 
 //^ 실시간 인기 프로그램 Swiper
 export const swiper2 = new Swiper('.mySwiper2', {
-  slidesPerView: 8,
-  slidesPerGroup: 8,
+  slidesPerView: 3,
+  slidesPerGroup: 3,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
   keyboard: {
     enabled: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+      slidesPerGroup: 5
+    },
+    1024: {
+      slidesPerView: 8,
+      slidesPerGroup: 8
+    }
   }
 })
 
 //^ 오직 티빙에만 있어요 Swiper
 export const swiper3 = new Swiper('.mySwiper3', {
-  slidesPerView: 7,
-  slidesPerGroup: 7,
+  slidesPerView: 2,
+  slidesPerGroup: 2,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
   },
   keyboard: {
     enabled: true
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 4
+    },
+    1024: {
+      slidesPerView: 7,
+      slidesPerGroup: 7
+    }
   }
   // autoplay: {
   // delay: 3500,
