fc563d99cd3e1cbe989fe64d2cf7c0c9eefee803
oweaj <qwe1520156@naver.com>
2023-02-08 23:29:12 +0900

    Fix : login 유효성 적용

--- pages/login/js/main.js
+++ pages/login/js/main.js
@@ -1,8 +1,12 @@
-import { getNode, attr } from '../../../client/lib/index.js'
+import { getNode, attr, getInputValue } from '../../../client/lib/index.js'
 
+const ID = getNode('#ID')
+const PW = getNode('#PW')
 const loginBtn = getNode('.login-btn')
 const showHideBtn = getNode('.show-hide')
 const clear = getNode('.deleteID')
+const ruleID = getNode('.ruleID')
+const rulePW = getNode('.rulePW')
 
 loginBtn.addEventListener('click', () => {
   if (loginBtn.getAttribute('onclick') === null) {
@@ -26,3 +30,25 @@ function showHide() {
   }
 }
 showHideBtn.addEventListener('click', showHide)
+
+ID.addEventListener('keyup', () => {
+  let value = getInputValue(ID)
+
+  if (value.includes('@') && value.includes('.')) {
+    ruleID.textContent = ''
+  } else {
+    ruleID.textContent = '올바른 로그인 형식이 아닙니다.'
+    ruleID.style.color = '#ff153c'
+  }
+})
+
+PW.addEventListener('keyup', () => {
+  let value = getInputValue(PW)
+
+  if (value.length >= 8 && value.length <= 15) {
+    rulePW.textContent = ''
+  } else {
+    rulePW.textContent = '올바른 비밀번호 형식이 아닙니다.'
+    rulePW.style.color = '#ff153c'
+  }
+})
